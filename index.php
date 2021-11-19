2bc96e9e2b834562ca19b9f7cd105205750e0387
unknown <pratamasarip99@gmail.com>
2021-11-19 09:05:52 +0700

    ini commit pertama kamu

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+      <!--ini adalah pembuka navbar-->
+      <div class="container">
+
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="#">PRATAMA SYARIEP HT</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hobi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cita-cita</a>
+        </li>
+    </ul>
+</div>
+</div>
+</nav>
+<!--ini adalah penutup-->
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+    <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/pratama.jpg" class="card-img-top" style="width: 9rem;">
+  <div class="card-body">
+    <p class="card-text">Pratama</p>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+      <h1>Selamat datang di website saya</h1>
+     <p>Perkenalkan nama saya Pratama Syariep HT,saya dari kelas XI-RPL disini saya belajar bootstrap </p>
+    </div>
+  </div>
+</div>
+</div>
+<!--Link Javascript-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
